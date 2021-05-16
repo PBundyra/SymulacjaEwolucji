e98af1e754301dfe0089b142f152d7f6afabf66c
Patryk Bundyra <pb429159@students.mimuw.edu.pl>
2021-05-16 15:53:36 +0200

    Poprawiony poprzedni commit XD

--- src/Symulacja.java
+++ src/Symulacja.java
@@ -5,5 +5,6 @@ public class Symulacja {
     public static void main(String[] args) throws FileNotFoundException {
 
         Ewolucja ewolucja = new Ewolucja(new File(args[0]), new File(args[1]));
+        ewolucja.symulujEwolucje();
     }
 }
